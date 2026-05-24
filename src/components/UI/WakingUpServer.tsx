2d5fb7f2a99b680c279b3a0930b3e9c6c081ca0b
Vladislav Korniushenko <vkornushenko@gmail.com>
2026-05-24 14:01:43 +0300

    minor improvments in css code

--- src/components/UI/WakingUpServer.tsx
+++ src/components/UI/WakingUpServer.tsx
@@ -2,18 +2,21 @@
 import styles from '@/components/UI/WakingUpServer.module.css';
 import Card from './Card';
 import Container from './Container';
+import { CiServer } from 'react-icons/ci';
 
 export default function WakingUpServer() {
-
   return (
     <div className={styles.overlay}>
       <Container>
         <Card>
-          <h2>Server is waking up…</h2>
+          <div className={styles.header_container}>
+            <CiServer size={24}/>
+            <h2> Server is waking up…</h2>
+          </div>
           <div>
             <p>
-              This may take up to 10 seconds. This delay occurs
-              because the app is hosted on a free-tier service.
+              This may take up to 10 seconds. Delay occurs because the app
+              is hosted on a free-tier service.
             </p>
           </div>
           <div className={styles.progress_bar}>
